7ffb81576a492ebeb976b47b8a23f427389aad33
Kevin Yu <ykevin98@gmail.com>
2022-04-18 21:34:55 -0500

    Added regions to Constant class

--- SunPOSData/Common/Constants.cs
+++ SunPOSData/Common/Constants.cs
@@ -2,16 +2,23 @@
 {
     public static class Constants
     {
+        #region TableName Static Class
+
         public static class TableName
         {
             public const string Categories = "Categories";
             public const string Restaurants = "Restaurants";
         }
 
+        #endregion
+
+        #region StoredProcedure Static Class
+
         public static class StoredProcedures
         {
             public const string GetMenu = "dbo.GetMenu @RestaurantName";
         }
-        
+
+        #endregion
     }
 }
